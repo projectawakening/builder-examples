413f6073966e79c99ada9c43077ef359fa6c95cb
CCP Legolas <toniya@ccpgames.com>
2024-09-24 00:38:04 +0000

    upgrade vending machine to latest mud version

--- vending-machine/packages/contracts/src/systems/vending_machine/constants.sol
+++ vending-machine/packages/contracts/src/systems/vending_machine/constants.sol
@@ -2,6 +2,6 @@
 pragma solidity >=0.8.21;
 
 // make sure this matches mud.config.ts namespace
-bytes14 constant VENDING_MACHINE_DEPLOYMENT_NAMESPACE = "test2";
+bytes14 constant VENDING_MACHINE_DEPLOYMENT_NAMESPACE = "test";
 
-bytes16 constant VENDING_MACHINE_SYSTEM_NAME = "VendingMachine";
+bytes16 constant VENDING_MACHINE_SYSTEM_NAME = "VendingMachineSy";
