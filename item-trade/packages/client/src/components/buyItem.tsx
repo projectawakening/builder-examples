e594f522f6997a4cbab555ec5f3d8d6a0000a082
ccp sondheim <32130807+rhubarbgarden@users.noreply.github.com>
2024-09-25 15:47:46 +0000

    feat: add basic item trade client

---
+++ item-trade/packages/client/src/components/buyItem.tsx
@@ -0,0 +1,137 @@
+import { useMUD } from "../MUDContext";
+import React, { useRef, useState } from "react";
+import { EveButton, EveInput } from "@eveworld/ui-components";
+import { formatEther, parseEther } from "viem";
+
+const BuyItem = React.memo(function BuyItem({
+	smartAssemblyId,
+}: {
+	smartAssemblyId: bigint;
+}) {
+	const [itemPriceWei, setItemPriceWei] = useState<number | undefined>();
+	const [itemQuantity, setItemQuantity] = useState<number | undefined>();
+	const [erc20Balance, setErc20Balance] = useState<number | undefined>();
+
+	const itemOutId = import.meta.env.VITE_ITEM_OUT_ID
+
+	const {
+		network: { walletClient },
+		systemCalls: {
+			setItemPrice,
+			getItemPriceData,
+			purchaseItem,
+			getErc20Balance
+		},
+	} = useMUD();
+
+	const fetchItemPriceData = async () => {
+		const itemPriceData = await getItemPriceData();
+		setItemPriceWei(Number(itemPriceData?.price ?? 0))
+	}
+
+	const itemPriceWeiValueRef = useRef(0);
+
+	const handleEdit = (
+		refString: React.MutableRefObject<number>,
+		eventString: number
+	): void => {
+		refString.current = eventString;
+	};
+
+	return (
+		<>
+			<div className="Quantum-Container my-4">
+				<div>STEP 2: Manage buying inventory item ID: {itemOutId}</div>
+				<div className="text-xs">
+				You can change this inventory item ID in the .env file
+				</div>
+
+				<div className="mt-4">STEP 2.1: Get item buy price</div>
+				<div className="flex items-center">
+					<EveButton
+						className="mr-2"
+						typeClass="tertiary"
+						onClick={async (event) => {
+							event.preventDefault();
+							fetchItemPriceData()
+						}}
+					>
+						Fetch
+					</EveButton>
+					<span className="text-xs">
+						{itemPriceWei ? `${formatEther(BigInt(itemPriceWei))} ether units` : "No item price set"}
+					</span>
+				</div>
+
+				<div className="mt-4">STEP 2.2: Set item price in ether units</div>
+				<div className="flex flex-col items-start gap-3">
+					<EveInput
+						inputType="numerical"
+						defaultValue={undefined}
+						fieldName={"Item price"}
+						onChange={(str) => handleEdit(itemPriceWeiValueRef, str as number)}
+					></EveInput>
+					<div>
+						<EveButton
+							typeClass="primary"
+							onClick={async (event) => {
+								event.preventDefault();
+								await setItemPrice(
+									smartAssemblyId,
+									itemOutId,
+									parseEther(itemPriceWeiValueRef.current.toString())
+								);
+								fetchItemPriceData()
+							}}
+						>
+							Set Item Price
+						</EveButton>
+					</div>
+				</div>
+			</div>
+
+			<div className="Quantum-Container my-4">
+				<div>STEP 3: Purchase Item</div>
+				<div className="flex items-center">
+					<EveButton
+						className="mr-2"
+						typeClass="tertiary"
+						onClick={async (event) => {
+							event.preventDefault();
+							const balance = await getErc20Balance(walletClient.account?.address)
+							setErc20Balance(Number(balance ?? 0))
+						}}
+					>
+						Get balance
+					</EveButton>
+					<span className="text-xs">
+						{erc20Balance ? `${formatEther(BigInt(erc20Balance))} ether units` : "Click fetch to get buyer ERC-20 balance"}
+					</span>
+				</div>
+				<div className="flex items-start flex-col gap-3">
+					<EveInput
+						inputType="numerical"
+						defaultValue={itemQuantity}
+						fieldName={"item quantity"}
+						onChange={(str) => setItemQuantity(Number(str))}
+					></EveInput>
+					<div>
+						<EveButton
+							typeClass="primary"
+							onClick={async (event) => {
+								event.preventDefault();
+								await purchaseItem(
+									smartAssemblyId,
+									itemOutId,
+									itemQuantity								)
+							}}
+						>
+							Purchase items
+						</EveButton></div>
+				</div>
+			</div>
+		</>
+	);
+});
+
+export default BuyItem;
---
+++ item-trade/packages/client/src/components/sellItem.tsx
@@ -0,0 +1,152 @@
+import { useMUD } from "../MUDContext";
+import React, { useRef, useState } from "react";
+import { EveButton, EveInput } from "@eveworld/ui-components";
+import { formatEther, parseEther } from "viem";
+
+const SellItem = React.memo(function SellItem({
+	smartAssemblyId,
+}: {
+	smartAssemblyId: bigint;
+}) {
+	const [itemStackMultiple, setItemStackMultiple] = useState<
+		number | undefined
+	>();
+	const [itemPriceWei, setItemPriceWei] = useState<number | undefined>();
+	const [sellQuantity, setSellQuantity] = useState<number | undefined>();
+	const [erc20Balance, setErc20Balance] = useState<number | undefined>();
+
+	const itemInId = import.meta.env.VITE_ITEM_IN_ID;
+
+	const {
+		network: { walletClient },
+		systemCalls: { setSellConfig, sellItem, getErc20Balance, getItemSellData },
+	} = useMUD();
+
+	const fetchItemSellData = async () => {
+		const sellPriceData = await getItemSellData();
+		setItemStackMultiple(Number(sellPriceData?.enforcedItemMultiple ?? 0));
+		setItemPriceWei(Number(sellPriceData?.tokenAmount ?? 0));
+	};
+
+	const itemMultipleValueRef = useRef(0);
+	const itemPriceWeiValueRef = useRef(0);
+
+	const handleEdit = (
+		refString: React.MutableRefObject<number>,
+		eventString: number
+	): void => {
+		refString.current = eventString;
+	};
+
+	return (
+		<>
+			<div className="Quantum-Container my-4">
+				<div>STEP 4: Manage selling inventory item ID: {itemInId}</div>
+				<div className="text-xs">
+					You can change this inventory item ID in the .env file
+				</div>
+
+				<div className="mt-4">
+					STEP 4.1: Get item sell price and config data
+				</div>
+				<div className="flex items-center">
+					<EveButton
+						className="mr-2"
+						typeClass="tertiary"
+						onClick={async (event) => {
+							event.preventDefault();
+							fetchItemSellData();
+						}}
+					>
+						Fetch
+					</EveButton>
+					<div className="flex flex-col">
+						<span className="text-xs">
+							{itemPriceWei && itemStackMultiple
+								? `Every ${itemStackMultiple} units of item ${itemInId} can be sold for ${formatEther(BigInt(itemPriceWei))} EVE`
+								: "No sell config set"}
+						</span>
+					</div>
+				</div>
+
+				<div className="mt-4">STEP 4.2: Set item price in ether units</div>
+				<div className="flex flex-col items-start gap-3">
+					<EveInput
+						inputType="numerical"
+						defaultValue={undefined}
+						fieldName={"Item multiple"}
+						onChange={(str) => handleEdit(itemMultipleValueRef, str as number)}
+					></EveInput>
+
+					<EveInput
+						inputType="numerical"
+						defaultValue={undefined}
+						fieldName={"Stack price"}
+						onChange={(str) => handleEdit(itemPriceWeiValueRef, str as number)}
+					></EveInput>
+					<div>
+						<EveButton
+							typeClass="primary"
+							onClick={async (event) => {
+								event.preventDefault();
+								await setSellConfig(
+									smartAssemblyId,
+									itemMultipleValueRef.current,
+									parseEther(itemPriceWeiValueRef.current.toString())
+								);
+								fetchItemSellData();
+							}}
+						>
+							Set Item Price
+						</EveButton>
+					</div>
+				</div>
+			</div>
+
+			<div className="Quantum-Container my-4">
+				<div>STEP 5: Sell Item</div>
+				<div className="flex items-center">
+					<EveButton
+						className="mr-2"
+						typeClass="tertiary"
+						onClick={async (event) => {
+							event.preventDefault();
+							// const balance = await getErc20Balance(
+							// 	walletClient.account?.address
+							// );
+							// setErc20Balance(Number(balance ?? 0));
+						}}
+					>
+						Get balance
+					</EveButton>
+					<span className="text-xs">
+						{erc20Balance
+							? `${formatEther(BigInt(erc20Balance))} ether units`
+							: "Click fetch to get player item balance"}
+					</span>
+				</div>
+				<div className="flex items-start flex-col gap-3">
+					<EveInput
+						inputType="numerical"
+						defaultValue={sellQuantity}
+						fieldName={"item quantity"}
+						onChange={(str) => setSellQuantity(Number(str))}
+					></EveInput>
+					<div>
+						<EveButton
+							typeClass="primary"
+							onClick={async (event) => {
+								event.preventDefault();
+								await sellItem(smartAssemblyId, sellQuantity);
+							}}
+						>
+							Sell items
+						</EveButton>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+});
+
+export default SellItem;
