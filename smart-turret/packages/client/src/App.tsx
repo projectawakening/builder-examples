9f480655733901debf440f2e05c4e05cfc89cddd
CCP Red Dragon <181853434+CCP-Red-Dragon@users.noreply.github.com>
2024-10-10 17:25:08 +0100

    Alterations

--- smart-turret/packages/client/src/App.tsx
+++ smart-turret/packages/client/src/App.tsx
@@ -1,7 +1,6 @@
 import { useMUD } from "./MUDContext";
 import React, { useEffect, useState, useRef } from "react";
 import { EveButton, EveInput, Header } from "@eveworld/ui-components";
-import { abbreviateAddress } from "@eveworld/utils";
 import { SmartCharacter } from "@eveworld/types";
 import "./styles.css";
 import "./styles-ui.css";
@@ -23,7 +22,6 @@ export const App = () => {
 	const {
 		network: { walletClient },
 		systemCalls: {
-			collectTokens,
 			getWhitelist,
 			addToWhitelist,
 			removeFromWhitelist
@@ -67,25 +65,6 @@ export const App = () => {
 		}, 1000);
 	}, [walletClient.account?.address]);
 
-	/*
-	if (!connected || !publicClient || !walletClient) {
-		return (
-			<>
-			<div>
-			<div className="h-full w-full bg-crude-5 -z-10">
-				<EveConnectWallet
-				handleConnect={handleConnect}
-				availableWallets={availableWallets}
-				/>
-				<GenerateEveFeralCodeGen style="top-12" />
-				<GenerateEveFeralCodeGen style="bottom-12" />
-			</div>
-			</div>
-		</>
-		);
-	}
-		*/
-
 	var arrayData = <WhitelistEntry id="LOADING...."></WhitelistEntry>;
 
 	const remove = async (id) => {
@@ -95,94 +74,83 @@ export const App = () => {
 			id
 		);
 
-		console.log("Whitelist;", whitelist)
-
 		loadWhitelist(whitelist);
 	}
 
 	async function fetchWhitelist(){		
 		const whitelist = await getWhitelist();
-
 		loadWhitelist(whitelist);
-	}
-
-	
+	}	
 
 	async function loadWhitelist(result){
-
-		console.log(arrayData);
 		arrayData = result.whitelist.map((value) => <WhitelistEntry id={value.toString()} handleClick={remove}>{value}</WhitelistEntry>)
-
 		handleListChange(arrayData);
-		console.log("ARRAY DATA: ", arrayData);
 	}
 
 	return (
-		<div id="root">
 		<div className="bg-crude-5 w-screen min-h-screen">
 			<div className="flex flex-col align-center max-w-[560px] mx-auto pb-6 min-h-screen h-full">
 				<div className="Quantum-Container my-4">
-				<header className="w-full items-center py-6 Custom-Title" id="header">
-					SMART TURRET WHITELISTING
-				</header>
+					<header className="w-full items-center py-6 Custom-Title" id="header">
+						SMART TURRET WHITELISTING
+					</header>
 				</div>
 				<div className="relative">
-				<div className="grid border border-brightquantum bg-crude">
-				<div className="flex flex-col align-center border border-brightquantum">
-				<div className="Quantum-Container font-semibold ">				
-					<div className="grid grid-cols-1 rows-2 gap-1">
-						<EveInput
-							inputType="string"
-							defaultValue=""
-							fieldName={"Character ID"}
-							onChange={(str) => handleEdit(characterIDRef, str as number)}
-						/>
-						<div></div>
-						<EveButton typeClass="primary"
-						onClick={async (event) => {
-							event.preventDefault();
+					<div className="grid border border-brightquantum bg-crude">
+					<div className="flex flex-col align-center border border-brightquantum">
+					<div className="Quantum-Container font-semibold ">				
+						<div className="grid grid-cols-1 rows-2 gap-1">
+							<EveInput
+								inputType="string"
+								defaultValue=""
+								fieldName={"Character ID"}
+								onChange={(str) => handleEdit(characterIDRef, str as number)}
+							/>
+							<div></div>
+							<EveButton typeClass="primary"
+							onClick={async (event) => {
+								event.preventDefault();
+								
+								const whitelist = await addToWhitelist(
+									characterIDRef.current
+								);
+
+								console.log("DATA", whitelist);
+
+								loadWhitelist(whitelist);
+							}}
+							className="primary-sm">
+								Add to Whitelist					
+							</EveButton>
 							
-							const whitelist = await addToWhitelist(
-								characterIDRef.current
-							);
-
-							console.log("DATA", whitelist);
-
-							loadWhitelist(whitelist);
-						}}
-						className="primary-sm">
-							Add to Whitelist					
-						</EveButton>
-						
+						</div>
 					</div>
-				</div>
-				<div className="Quantum-Container font-semibold">				
+					<div className="Quantum-Container font-semibold">				
+						<div>
+							<EveButton typeClass="primary"
+							onClick={async (event) => {
+								event.preventDefault();
+								fetchWhitelist();
+							}}
+							className="primary-sm">
+								Fetch Whitelist						
+							</EveButton>
+						</div>
+					</div>
+					
+					<div className="Quantum-Container font-semibold">				
+					<div className="w-full items-center py-1" id="header">
+						Whitelist
+					
+					</div>
 					<div>
-						<EveButton typeClass="primary"
-						onClick={async (event) => {
-							event.preventDefault();
-							fetchWhitelist();
-						}}
-						className="primary-sm">
-							Fetch Whitelist						
-						</EveButton>
+					{listVar}
 					</div>
-				</div>
-				
-				<div className="Quantum-Container font-semibold">				
-				<div className="w-full items-center py-1" id="header">
-					Whitelist
-				
-				</div>
-				<div>
-				{listVar}
-				</div>
-				</div>				
-				</div>
-				</div>
+					</div>				
+					</div>
+					</div>
 				</div>
 			</div>
 		</div>
-    </div>
 	);
 };
