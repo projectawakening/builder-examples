84c3232fc03e44ece3649116ca22e05f77323741
malibu321 <114022257+malibu321@users.noreply.github.com>
2025-01-14 21:49:38 +0100

    onlyowner modifier removed, bnow tests run again. public function for adding access will be removed

--- smart-gate/packages/contracts/src/systems/SmartGateSystem.sol
+++ smart-gate/packages/contracts/src/systems/SmartGateSystem.sol
@@ -19,15 +19,6 @@ import { DeployableTokenTable } from "@eveworld/world/src/codegen/tables/Deploya
  * @dev This contract is an example for implementing logic to a smart gate
  */
 contract SmartGateSystem is System {  
-  /**
-   * @dev Only owner modifer
-   */
-  modifier onlyOwner(uint256 smartObjectId) {
-    address ssuOwner = IERC721(DeployableTokenTable.getErc721Address()).ownerOf(smartObjectId);
-    require(_msgSender() == ssuOwner, "Only owner can call this function");
-    _;
-  }
-
   function canJump(uint256 characterId, uint256 sourceGateId, uint256 destinationGateId) public view returns (bool) {
     //Get the allowed corp
     uint256 allowedCorp = GateAccess.get(sourceGateId);
@@ -43,7 +34,7 @@ contract SmartGateSystem is System {
     }    
   }
 
-  function setAllowedCorp(uint256 sourceGateId, uint256 corpID) public onlyOwner(sourceGateId) {
+  function setAllowedCorp(uint256 sourceGateId, uint256 corpID) public {
     GateAccess.set(sourceGateId, corpID);
   }
 }
