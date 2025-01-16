8f137f3c407d0100bdc706ed463a8fcf5220961d
malibu321 <114022257+malibu321@users.noreply.github.com>
2025-01-16 13:12:49 +0100

    prepare for Access Lists Edit functions

--- smart-gate/packages/contracts/src/systems/SmartGateSystem.sol
+++ smart-gate/packages/contracts/src/systems/SmartGateSystem.sol
@@ -20,6 +20,15 @@ import { AccessLists, AccessListsData } from "../codegen/tables/AccessLists.sol"
  * @dev This contract is an example for implementing logic to a smart gate
  */
 contract SmartGateSystem is System {  
+  /**
+   * @dev Only owner modifer
+   */
+  modifier onlyOwner(uint256 smartObjectId) {
+    address ssuOwner = IERC721(DeployableTokenTable.getErc721Address()).ownerOf(smartObjectId);
+    require(_msgSender() == ssuOwner, "Only owner can call this function");
+    _;
+  }  
+
   function canJump(uint256 characterId, uint256 sourceGateId, uint256 /*_destinationGateId*/) public view returns (bool) {
     return hasCharAccessToSmartObject(characterId, sourceGateId);
   }
@@ -53,4 +62,94 @@ contract SmartGateSystem is System {
 
     return false;
   }
+
+/*
+function createAccessList(string memory accessListName, bool isWhiteList) public onlyAccessListManager {
+    AccessListsData memory existingList = AccessLists.get(accessListName);
+    require(bytes(existingList.accessListName).length == 0, "Access list already exists");
+
+    AccessListsData memory newList;
+    newList.accessListName = accessListName;
+    newList.isWhiteList = isWhiteList;
+    newList.CorpIds = new uint256;
+    nt.CharIds = new uint256;
+
+    Accets.set(accessListName, newList);
+  }
+
+  function deleteAccessList(string memory accessListName) public onlyOwner(sourceGateId) {
+    AccessListsData memory existingList = AccessLists.get(accessListName);
+    require(bytes(existingList.accessListName).length > 0, "Access list does not exist");
+    AccessLists.deleteRecord(accessListName);
+  }
+
+  function addAccessListToGate(string memory accessListName, uint256 gateId) public onlyOwner(sourceGateId) {
+    AccessListsData memory existingList = AccessLists.get(accessListName);
+    require(bytes(existingList.accessListName).length > 0, "Access list does not exist");
+
+    string[] memory accessListIds = GateAccess.get(gateId).accessListIds;
+    GateAccess.set(gateId, addElementToArray(accessListIds, accessListName));
+  }
+
+  function removeAccessListFromGate(string memory accessListName, uint256 gateId) public onlyOwner(sourceGateId) {
+    require(bytes(AccessLists.get(accessListName).accessListName).length > 0, "Access list does not exist");
+
+    string[] memory accessListIds = GateAccess.get(gateId).accessListIds;
+    GateAccess.set(gateId, removeElementWithValueFromArray(accessListIds, accessListName));
+  }
+
+    function addCharIdToAccessList(uint256 characterId, string memory accessListName) public onlyOwner(sourceGateId) {
+      AccessListsData memory accessListsData = AccessLists.get(accessListName);
+      require(bytes(accessListsData.accessListName).length > 0, "Access list does not exist");
+      accessListsData.CharIds = addElementToArray(accessListsData.CharIds, characterId);
+      AccessLists.set(accessListName, accessListsData);
+  }
+
+  function removeCharIdFromAccessList(uint256 characterId, string memory accessListName) public onlyOwner(sourceGateId) {
+      AccessListsData memory accessListsData = AccessLists.get(accessListName);
+      require(bytes(accessListsData.accessListName).length > 0, "Access list does not exist");
+      accessListsData.CharIds = removeElementWithValueFromArray(accessListsData.CharIds, characterId);
+      AccessLists.set(accessListName, accessListsData);
+  }
+
+  function addCorpIdToAccessList(uint256 corpId, string memory accessListName) public onlyOwner(sourceGateId) {
+      AccessListsData memory accessListsData = AccessLists.get(accessListName);
+      require(bytes(accessListsData.accessListName).length > 0, "Access list does not exist");
+      accessListsData.CorpIds = addElementToArray(accessListsData.CorpIds, corpId);
+      AccessLists.set(accessListName, accessListsData);
+  }
+
+  function removeCorpIdFromAccessList(uint256 corpId, string memory accessListName) public onlyOwner(sourceGateId) {
+      AccessListsData memory accessListsData = AccessLists.get(accessListName);
+      require(bytes(accessListsData.accessListName).length > 0, "Access list does not exist");
+      accessListsData.CorpIds = removeElementWithValueFromArray(accessListsData.CorpIds, corpId);
+      AccessLists.set(accessListName, accessListsData);
+  }
+
+  function addElementToArray(string[] memory array, string memory newElement) internal pure returns (string[] memory) {
+    string[] memory updatedArray = new string[](array.length + 1);
+    for (uint256 i = 0; i < array.length; i++) {
+      updatedArray[i] = array[i];
+    }
+    updatedArray[array.length] = newElement;
+    return updatedArray;
+  }
+
+  function removeElementWithValueFromArray(string[] memory array, string memory value) internal pure returns (string[] memory) {
+    uint256 count = 0;
+    for (uint256 i = 0; i < array.length; i++) {
+      if (keccak256(bytes(array[i])) != keccak256(bytes(value))) count++;
+    }
+    string[] memory updatedArray = new string[](count);
+    uint256 index = 0;
+    for (uint256 i = 0; i < array.length; i++) {
+      if (keccak256(bytes(array[i])) != keccak256(bytes(value))) {
+        updatedArray[index] = array[i];
+        index++;
+      }
+    }
+    return updatedArray;
+  }
+*/
 }
+
