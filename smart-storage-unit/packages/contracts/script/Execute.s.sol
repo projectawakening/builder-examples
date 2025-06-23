04296857d314e316e676334702315ef65e21ff45
CCP Red Dragon <181853434+CCP-Red-Dragon@users.noreply.github.com>
2025-06-23 10:14:39 +0000

    ssu access issue fix (#108)
    Co-authored-by: CCP Legolas <toniya@ccpgames.com>

--- smart-storage-unit/packages/contracts/script/Execute.s.sol
+++ smart-storage-unit/packages/contracts/script/Execute.s.sol
@@ -6,110 +6,72 @@ import { ResourceId, WorldResourceIdLib } from "@latticexyz/world/src/WorldResou
 import { IBaseWorld } from "@latticexyz/world/src/codegen/interfaces/IBaseWorld.sol";
 import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
 import { Systems } from "@latticexyz/world/src/codegen/tables/Systems.sol";
+import { IWorldWithContext } from "@eveworld/smart-object-framework-v2/src/IWorldWithContext.sol";
 
 import { InventorySystem, inventorySystem } from "@eveworld/world-v2/src/namespaces/evefrontier/codegen/systems/InventorySystemLib.sol";
-import { RatioConfig } from "../src/codegen/tables/RatioConfig.sol";
-import { IWorld } from "../src/codegen/world/IWorld.sol";
-import { Utils } from "../src/systems/Utils.sol";
-import { SmartStorageUnitSystem, smartStorageUnitSystem } from "../src/systems/SmartStorageUnitSystem.sol";
 import { EphemeralInteractSystem, ephemeralInteractSystem } from "@eveworld/world-v2/src/namespaces/evefrontier/codegen/systems/EphemeralInteractSystemLib.sol";
 import { OwnershipSystem, ownershipSystem } from "@eveworld/world-v2/src/namespaces/evefrontier/codegen/systems/OwnershipSystemLib.sol";
-
 import { InventoryItem, InventoryItemData } from "@eveworld/world-v2/src/namespaces/evefrontier/codegen/tables/InventoryItem.sol";
 import { EphemeralInvItem, EphemeralInvItemData } from "@eveworld/world-v2/src/namespaces/evefrontier/codegen/tables/EphemeralInvItem.sol";
 import { Tenant, Characters, CharactersByAccount, EntityRecord } from "@eveworld/world-v2/src/namespaces/evefrontier/codegen/index.sol";
+import { InventoryItemParams } from "@eveworld/world-v2/src/namespaces/evefrontier/systems/inventory/types.sol";
 import { ObjectIdLib } from "@eveworld/world-v2/src/namespaces/evefrontier/libraries/ObjectIdLib.sol";
+
+import { SmartStorageUnitSystem, smartStorageUnitSystem } from "../src/systems/SmartStorageUnitSystem.sol";
+
+import { RatioConfig } from "../src/codegen/tables/RatioConfig.sol";
+import { IWorld } from "../src/codegen/world/IWorld.sol";
+import { Utils } from "../src/systems/Utils.sol";
 import { RatioConfigData } from "../src/codegen/tables/RatioConfig.sol";
 
-import { InventoryItemParams } from "@eveworld/world-v2/src/namespaces/evefrontier/systems/inventory/types.sol";
-
 contract Execute is Script {
-  //Player
-  uint256 playerPrivateKey;
-  address player;
+  uint256 SSU_TYPE_ID = 77917;
+  uint256 ssuItemId = 565656565;
 
-  //Admin
-  uint256 adminPrivateKey;
-  address admin;
-
-  //SSU ID
-  uint256 smartStorageUnitId;
-
-  //Items
-  uint256 itemIn; 
-  uint256 itemOut;
-
-  //Testing
-  uint64 testQuantityIn;
-
-  function ConsoleLogInventories(uint256 itemInSmartObjectId, uint256 itemOutSmartObjectId) view internal {
-    EphemeralInvItemData memory ephInvInItem = EphemeralInvItem.get(smartStorageUnitId, player, itemInSmartObjectId);
-    console.log("[EPHEMERAL] Player's Ephemeral Inventory [Item In]: ", vm.toString(ephInvInItem.quantity));
-
-    EphemeralInvItemData memory ephInvOutItem = EphemeralInvItem.get(smartStorageUnitId, player, itemOutSmartObjectId);
-    console.log("[EPHEMERAL] Player's Ephemeral Inventory [Item Out]: ", vm.toString(ephInvOutItem.quantity));
-
-    InventoryItemData memory invItemOut = InventoryItem.get(smartStorageUnitId, itemOutSmartObjectId);
-    console.log("[INVENTORY] Admins Inventory [Item Out]: ", vm.toString(invItemOut.quantity));
-
-    InventoryItemData memory invItemIn = InventoryItem.get(smartStorageUnitId, itemInSmartObjectId);
-    console.log("[INVENTORY] Admins Inventory [Item In]: ", vm.toString(invItemIn.quantity));
-  }
-
+  IWorldWithContext world;
   function run(address worldAddress) external {
-    adminPrivateKey = vm.envUint("PRIVATE_KEY");
-    admin = vm.addr(adminPrivateKey);
+    uint256 adminPrivateKey = vm.envUint("PRIVATE_KEY");
+    address admin = vm.addr(adminPrivateKey);
 
-
-    playerPrivateKey = vm.envUint("TEST_PLAYER_PRIVATE_KEY");
-    player = vm.addr(playerPrivateKey);
-
-    vm.startBroadcast(playerPrivateKey);
+    uint256 playerPrivateKey = vm.envUint("TEST_PLAYER_PRIVATE_KEY");
+    address player = vm.addr(playerPrivateKey);
 
     StoreSwitch.setStoreAddress(worldAddress);
-    IBaseWorld world = IBaseWorld(worldAddress);
-
-    bytes32 tenantId = Tenant.getTenantId();
-
-    //Read from .env
-    smartStorageUnitId = vm.envUint("SSU_ID");
-    itemIn = vm.envUint("ITEM_IN_TYPE_ID");
-    itemOut = vm.envUint("ITEM_OUT_TYPE_ID");
-    testQuantityIn = uint64(vm.envUint("EXECUTE_QUANTITY"));
-
-    uint256 itemInSmartObjectId = ObjectIdLib.calculateNonSingletonId(tenantId, itemIn);
-    uint256 itemOutSmartObjectId = ObjectIdLib.calculateNonSingletonId(tenantId, itemOut);
+    world = IWorldWithContext(worldAddress);
 
     ResourceId systemId = Utils.smartStorageUnitSystemId();
+    // address contractAddress = smartStorageUnitSystem.getAddress();
 
-    ConsoleLogInventories(itemInSmartObjectId, itemOutSmartObjectId);
+    // console.log("Contract Address: ", contractAddress);
 
-    RatioConfigData memory ratioConfigData = RatioConfig.get(smartStorageUnitId, itemInSmartObjectId);
-    console.log("Ratio: ", vm.toString(ratioConfigData.ratioIn), " : ", vm.toString(ratioConfigData.ratioOut));
+    bytes32 tenantId = Tenant.getTenantId();
+    uint256 smartStorageUnitId = ObjectIdLib.calculateObjectId(tenantId, ssuItemId);
 
+    vm.startBroadcast(adminPrivateKey);
     (address contractAddress, ) = Systems.get(systemId);
-
+    console.log("System Address: ", contractAddress);
+    ephemeralInteractSystem.setTransferFromEphemeralAccess(smartStorageUnitId, address(contractAddress), true);
     vm.stopBroadcast();
 
     vm.startBroadcast(adminPrivateKey);
-
-    console.log("Setting transfer from ephemeral access");
-    ephemeralInteractSystem.setTransferFromEphemeralAccess(smartStorageUnitId, contractAddress, true);
-    console.log("Transfer from ephemeral access set");
-
+    _execute(tenantId, smartStorageUnitId, player);
     vm.stopBroadcast();
+  }
 
-    vm.startBroadcast(playerPrivateKey);
+  function _execute(bytes32 tenantId, uint256 smartStorageUnitId, address player) private {
+    uint256 itemIn = vm.envUint("ITEM_IN_TYPE_ID");
+    uint64 testQuantityIn = uint64(vm.envUint("EXECUTE_QUANTITY"));
 
-    world.call(systemId, 
-      abi.encodeCall(
-        SmartStorageUnitSystem.execute, 
-        (smartStorageUnitId, testQuantityIn, itemInSmartObjectId)
-      )
+    uint256 itemInSmartObjectId = ObjectIdLib.calculateObjectId(tenantId, itemIn);
+
+    console.log("Item In Smart Object ID: ", itemInSmartObjectId);
+
+    ResourceId systemId = Utils.smartStorageUnitSystemId();
+
+    world.callFrom(
+      player,
+      systemId,
+      abi.encodeCall(SmartStorageUnitSystem.execute, (smartStorageUnitId, testQuantityIn, itemInSmartObjectId))
     );
-
-    ConsoleLogInventories(itemInSmartObjectId, itemOutSmartObjectId);
-
-    vm.stopBroadcast();
   }
 }
